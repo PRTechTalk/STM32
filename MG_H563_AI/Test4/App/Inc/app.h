23209169775f607fa2fda0871d7b8c9bc6bb8016
Peter Ranemyr <ranemyr@gmail.com>
2026-08-19 09:40:30 +0200

    Deleted a older test project

--- MG_H563_AI/Test4/App/Inc/app.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void App_EthernetInit(void);
-void App_Init(void);
-void App_Poll(void);
-
--- MG_H563_AI/Test4/tools/pack_web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-NODE_BIN=${NODE_BIN:-node}
-"$NODE_BIN" Mongoose/pack.js \
-  Web/index.html:index.html \
-  Web/app.css:app.css \
-  Web/app.js:app.js \
-  "Logotype/PR Tech Talk 1024_549.jpg:logo.jpg" > App/Src/packed_fs.c
-
